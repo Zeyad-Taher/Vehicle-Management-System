2e9d1603b58b56181d2c728b2c8280a6906f1581
zeyad taher <zeyadtaher16@gmail.com>
2022-09-08 05:40:54 +0200

    extracting layout, dummy pages for 6 departments

---
+++ src/main/java/com/example/findcar/ApplicationController.java
@@ -0,0 +1,36 @@
+package com.example.findcar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/accounts")
+    public String accounts(){
+        return "accounts/index";
+    }
+    @GetMapping("/helpdesk")
+    public String helpdesk(){
+        return "helpdesk/index";
+    }
+    @GetMapping("/hr")
+    public String hr(){
+        return "hr/index";
+    }
+    @GetMapping("/parameters")
+    public String parameters(){
+        return "parameters/index";
+    }
+    @GetMapping("/payroll")
+    public String payroll(){
+        return "payroll/index";
+    }
+    @GetMapping("/vehicle")
+    public String vehicle(){
+        return "vehicle/index";
+    }
+}
