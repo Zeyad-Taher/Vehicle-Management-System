b9c81aab917d4d6f301fa39d91901e1c01852c0a
zeyad taher <zeyadtaher16@gmail.com>
2022-09-18 15:59:09 +0200

    added add country buttom and form page

--- src/main/java/com/example/vehiclemanagementsystem/parameters/controller/CountryController.java
+++ src/main/java/com/example/vehiclemanagementsystem/parameters/controller/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -23,5 +24,10 @@ public class CountryController {
     @GetMapping("/countryAdd")
     public String addCountry(){
         return "parameters/countryAdd";
+    }
+    @PostMapping("/countries")
+    public String save(Country country) {
+        countryService.save(country);
+        return "redirect:/countries";
     }
 }
--- src/main/java/com/example/vehiclemanagementsystem/parameters/service/CountryService.java
+++ src/main/java/com/example/vehiclemanagementsystem/parameters/service/CountryService.java
@@ -15,4 +15,8 @@ public class CountryService {
     public List<Country> getAll(){
         return (List<Country>) countryRepository.findAll();
     }
+
+    public void save(Country country){
+        countryRepository.save(country);
+    }
 }
